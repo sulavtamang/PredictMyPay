1cba94fa562ced83090e656d37959f55a26ed0db
Sulav Tamang <ta.sulav.man.singh22@ncmt.edu.np>
2025-07-31 14:23:22 +0545

    update page config file

--- app/utils/page_config.py
+++ app/utils/page_config.py
@@ -1,9 +1,14 @@
 import streamlit as st
+from utils.path_helper import get_asset_path
+from utils.image_loader import load_image_base64
 
 def set_page_config():
+    icon_path = get_asset_path('logo.svg')
+    icon_src = load_image_base64(icon_path)
+
     st.set_page_config(
         page_title="PredictMyPay",
-        page_icon="assets/logo.svg",
+        page_icon=icon_src,
         layout="wide",  # ✅ Removes default padding and makes it full-width
         initial_sidebar_state="collapsed"  # Optional: hide sidebar by default
     )
